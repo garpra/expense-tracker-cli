8093af844d340a905e37e583e8ba3a3518b04098
Gilang Arya Pradana <gilangarya272001@gmail.com>
2025-09-11 20:11:29 +0700

    feat: add database initialization function

--- db.py
+++ db.py
@@ -4,3 +4,28 @@ def get_connection():
   conn = sqlite3.connect("expenses.db")
   conn.row_factory = sqlite3.Row
   return conn
+
+def init_db():
+  conn = get_connection()
+  cursor = conn.cursor()
+
+  cursor.execute("""
+    CREATE TABLE IF NOT EXISTS expenses (
+      id       INTEGER PRIMARY KEY AUTOINCREMENT,
+      amount   REAL NOT NULL,
+      category TEXT NOT NULL,
+      note     TEXT,
+      date     TEXT NOT NULL
+    )
+  """)
+  cursor.execute("""
+    CREATE TABLE IF NOT EXISTS budgets (
+      category  TEXT NOT NULL,
+      month     TEXT NOT NULL,
+      amount    REAL NOT NULL,
+      PRIMARY KEY (category, month)
+    )
+  """)
+
+  conn.commit()
+  conn.close()
