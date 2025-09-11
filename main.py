15d7abca6a62ba4165976d36dc541a124a1278d3
Gilang Arya Pradana <gilangarya272001@gmail.com>
2025-09-12 06:50:11 +0700

    feat: add CLI command to list expenses

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import typer
-from db import init_db, add_expense
+from db import init_db, add_expense, get_expenses
 from rich.console import Console
+from rich.table import Table
 
 app = typer.Typer()
 console = Console()
@@ -23,6 +24,30 @@ def add():
   
   add_expense(amount, category, note)
   console.print(f"Added: {amount:,.0f} — {category}".replace(",", "."))
+
+@app.command("list")
+def show():
+  rows = get_expenses()
+
+  if(len(rows) == 0):
+    console.print("[red]Belum ada pengeluaran[/red]")
+    return
+
+  table = Table()
+  table.add_column("Tanggal", style="dim")
+  table.add_column("Kategori")
+  table.add_column("Nominal", justify="right")
+  table.add_column("Note")
+
+  for row in rows:
+    date = row["date"]
+    category = row["category"]
+    amount = row["amount"]
+    note = row["note"] or ""
+
+    table.add_row(date, category, f"Rp {amount:,.0f}".replace(",", "."), note)
+
+  console.print(table)
 
 if __name__ == "__main__":
     app()
