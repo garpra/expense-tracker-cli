d45c9e154f44178218ddcec11fb7305d2895d045
Gilang Arya Pradana <gilangarya272001@gmail.com>
2025-09-13 06:49:37 +0700

    feat: add CLI command to display monthly budgets

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import typer
-from db import init_db, add_expense, get_expenses
+from db import init_db, add_expense, get_expenses, get_budgets
 from rich.console import Console
 from rich.table import Table
+from datetime import datetime
 
 app = typer.Typer()
 console = Console()
@@ -46,6 +47,33 @@ def show():
     note = row["note"] or ""
 
     table.add_row(date, category, f"{amount:,.0f}".replace(",", "."), note)
+
+  console.print(table)
+
+budget_app = typer.Typer()
+app.add_typer(budget_app, name="budget")
+
+@budget_app.command("set")
+def budget_set():
+  pass
+
+@budget_app.command("show")
+def budget_show():
+  month_now = datetime.now().strftime("%Y-%m")
+  rows = get_budgets(month_now)
+
+  if(len(rows) == 0):
+    console.print("[red]Belum ada budget[/red]")
+    return
+  
+  table = Table()
+  table.add_column("Kategori")
+  table.add_column("Nominal Budget")
+
+  for row in rows:
+    category = row["category"]
+    budget = row["amount"]
+    table.add_row(category, f"Rp {budget:,.0f}".replace(",", "."))
 
   console.print(table)
 
